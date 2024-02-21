60d53eb2a5c180eafa149425968220994d41457e
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-21 15:40:57 +0100

    Imposto axios in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
+/* 
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista (non ci interessa che siano tutti diversi).
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
 const { createApp } = Vue
 
 createApp({
     data(){
         return{
-
+            emails: null,
         }
     },
+
+    // al acricamento della pagina volgio che mi prenda le mail
+    mounted(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(() => {
+            
+        });
+        
+    }
 }).mount('#app')
